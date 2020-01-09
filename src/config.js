55d48bca3b8c210b01b6ee1bcab7a0791ae249c6
wendi <wendi@ruparupa.com>
2020-01-09 23:27:44 +0700

    fix base URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   universityURL: "https://cors-anywhere.herokuapp.com/http://universities.hipolabs.com/",
-  baseURL: "https://stupefied-lichterman-dfea71.netlify.com"
+  baseURL: "https://stupefied-lichterman-dfea71.netlify.com/"
 };
 
 export default config;
